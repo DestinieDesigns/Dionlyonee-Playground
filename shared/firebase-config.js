42626156eeaa4042bbca6ae0d483ed970ec2634c
DestinieDesigns <Destiniedesigns123@gmail.com>
2026-09-01 18:59:12 -0400

    Update Firebase configuration with new project details

--- shared/firebase-config.js
+++ shared/firebase-config.js
@@ -1,5 +1,5 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
 import {getDatabase} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-database.js";
 // Paste your Firebase WEB APP configuration here.
-const firebaseConfig={apiKey:"PASTE_YOUR_KEY",authDomain:"PASTE_YOUR_PROJECT.firebaseapp.com",databaseURL:"https://PASTE_YOUR_PROJECT-default-rtdb.firebaseio.com",projectId:"PASTE_YOUR_PROJECT",storageBucket:"PASTE_YOUR_PROJECT.firebasestorage.app",messagingSenderId:"PASTE_YOUR_SENDER_ID",appId:"PASTE_YOUR_APP_ID"};
+const firebaseConfig={apiKey:"AIzaSyDx1NPIWO_z7vowVKsViqZOdo-LOYPPpPI",authDomain:"dionlyonee-game-test.firebaseapp.com",databaseURL:"https://dionlyonee-game-test-default-rtdb.firebaseio.com",projectId:"dionlyonee-game-test",storageBucket:"dionlyonee-game-test.firebasestorage.app",messagingSenderId:"297655548183",appId:"1:297655548183:web:8703736174f73d99485f52"};
 const app=initializeApp(firebaseConfig);const database=getDatabase(app);export{app,database};
